6034a72b1d779b8f0f52f23b5c7fbf26a34890ac
sebug <sebastian.gfeller@gmail.com>
2019-08-31 10:30:46 +0200

    Adding references

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import style from './style.css';
+import dc from 'dc';
+import crossfilter from 'crossfilter2';
+import * as d3 from 'd3';
+
+console.log(dc);
+console.log(crossfilter);
+console.log(d3);
+
 console.log('Ohai DC');
