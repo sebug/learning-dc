babf43df3e922fdf57cadde50856a6b0e1a76158
sebug <sebastian.gfeller@gmail.com>
2019-08-31 10:49:50 +0200

    Ensuring DC.css is included as well

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import dc from 'dc';
 import crossfilter from 'crossfilter2';
 import * as d3 from 'd3';
 import paymentData from './payment-data.js';
+import dcCSS from 'dc/dc.css';
 
 console.log(paymentData);
