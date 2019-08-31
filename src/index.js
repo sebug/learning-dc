280b06d119d43bea42c766000e52dad274fdf98d
sebug <sebastian.gfeller@gmail.com>
2019-08-31 10:39:15 +0200

    Adding payment data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import style from './style.css';
 import dc from 'dc';
 import crossfilter from 'crossfilter2';
 import * as d3 from 'd3';
+import paymentData from './payment-data.js';
 
-console.log(dc);
-console.log(crossfilter);
-console.log(d3);
-
-console.log('Ohai DC');
+console.log(paymentData);
